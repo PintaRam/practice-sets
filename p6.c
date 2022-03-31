7192ad873cae6915d2455636bdef60a9a111e778
Pinta Ram Ram <97089550+PintaRam@users.noreply.github.com>
2022-03-31 16:49:49 +0000

    p6

--- p6.c
+++ p6.c
@@ -1,2 +1,43 @@
-p7.c
+#include<stdio.h>
+int input_array_size()
+{
+  int n;
+  printf("enter the array size\n");
+  scanf("%d",&n);
+  return n;
+}
+void input_array(int n, int a[n])
+{ int i;  
+  printf("enter the array element\n");
+  for(i=1;i<=n;i++)
+  scanf("%d",&a[i]);
+}
+
+int sum_n_arrays(int n, int a[n])
+{
+  int sum=0, i;
+  for(i=1;i<=n;i++)
+  sum=sum+a[i];
+  return sum;
+}
+void output(int n, int a[n], int sum)
+{
+  printf("sum of");
+   for(int i=1;i<=n;i++)
+ {
+  printf(" %d+ ", a[i]);
+  }
+printf("=%d\n",sum);
+}
+
+int main()
+{
+  int sum,n;
+  n=input_array_size();
+  int a[n];
+  input_array(n, a);
+  sum=sum_n_arrays( n,  a);
+  output(n,  a,  sum);
+  return 0;
+}
   
