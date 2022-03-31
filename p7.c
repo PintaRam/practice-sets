fe7eccc0547027530948af879873aef221ab0be3
Pinta Ram Ram <97089550+PintaRam@users.noreply.github.com>
2022-03-31 16:50:31 +0000

    p7

--- p7.c
+++ p7.c
@@ -1,2 +1,34 @@
-p8.c
+#include<stdio.h>
+#include<math.h>
+float input()
+{
+  float n;
+  printf("enter the value of n\n");
+  return n;
+  scanf("%f",&n);
+}
+float my_sqrt(float n)
+{ float x = n/2;
+  float sqrt;
+  sqrt = 0.5*( x+(n/x));
+  while (fabs(x-sqrt)>0.00001)
+  {
+    x=sqrt;
+    sqrt=0.5*(x+(n/x));
+  }
+  return sqrt;
+}
+void output(float n, float sqrt_result)
+{
+  printf("square root of a %f is=%f\n",n, sqrt_result);
+
+}
+int main()
+{float n, sqrt_result;
+n= input();
+sqrt_result= my_sqrt(n);
+output(n,sqrt_result);
+return 0;
+}
+
   
