2b2133634ae961d4aebd4a0020c507176fd1bb53
Pinta Ram Ram <97089550+PintaRam@users.noreply.github.com>
2022-03-31 16:48:10 +0000

    p4

---
+++ p4.c
@@ -0,0 +1,35 @@
+/*program to find largest amongest three numbers using pass by reference*/
+#include<stdio.h>
+
+void input(int *a, int*b, int*c)
+{
+  printf("enter the number\n");
+  scanf("%d%d%d",a,b,c);
+
+}
+void cmp(int a, int b, int c, int*largest)
+{
+if(a>b && a>c)
+    
+      *largest = a;
+else
+if(b>a && b>c)
+   
+     *largest= b;
+ else
+     *largest= c;
+      
+}
+void output(int a, int b, int c,int largest)
+{
+ printf("largest among the %d, %d and %d is=%d\n",a,b, c,largest);
+
+}
+int main()
+{
+  int a,b,c, largest;
+ input(&a,&b,&c);
+ cmp(a, b, c, &largest);
+output(a, b, c, largest);
+return 0;
+}
